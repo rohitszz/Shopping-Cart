8935730f43c5488234c22c2fdbe5644f04bdca19
rohitszz <rohitchoudhary0212u@gmail.com>
2025-10-31 23:00:17 +0530

    Add wishlist feature to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { FaShoppingCart } from "react-icons/fa";
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { FaBookSkull } from 'react-icons/fa6';
+import { FaCertificate } from 'react-icons/fa6';
 
 
 const Navbar = () => {
     const cart = useSelector( (state) => state.cart);
+    const wishlist = useSelector( (state) => state.wishlist);
   return (
     <div>
     <div className='flex flex-row justify-between items-center h-20 max-w-6xl mx-auto '>
@@ -14,6 +17,14 @@ const Navbar = () => {
         <img src='https://codehelp-shopping-cart.netlify.app/logo.png' className='h-14'/>
     </div>
     </NavLink>
+
+    <NavLink to="/wishlist">
+      <FaBookSkull className='text-[white] translate-x-[-11rem] text-2xl'/>
+      {
+        wishlist.length > 0  && <span className='absolute left-[30rem] top-[1.4rem] bg-green-600 text-xs w-5 h-5 flex justify-center items-center animate-bounce rounded-full text-white'>{wishlist.length}</span>
+      }
+    </NavLink>
+    <FaCertificate className='text-[white] text-2xl translate-x-[-24rem]'/>
     <div className='text-white flex items-center font-medium mr-5 space-x-6'>
     <NavLink to="/">
          <p>Home</p>
