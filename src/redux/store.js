26443b7ed8482ad11419c3583e87454edf71cee5
rohitszz <rohitchoudhary0212u@gmail.com>
2025-10-31 22:58:25 +0530

    Add wishlist slice to Redux store configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { CartSlice } from "./Slices/CartSlice";
+import { wishlistSlice } from "./Slices/WishlistSlice";
 
 export const store = configureStore({
     reducer:{
         cart: CartSlice.reducer,
+        wishlist:wishlistSlice.reducer,
     }
 })
