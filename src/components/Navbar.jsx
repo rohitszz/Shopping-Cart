adb835263aab679c2192a49319b40358d37f5e57
rohitszz <rohitchoudhary0212u@gmail.com>
2025-11-24 02:34:26 +0530

    Implement theme toggle in Navbar
    Added theme toggle functionality to Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaShoppingCart } from "react-icons/fa";
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { FaBookSkull } from 'react-icons/fa6';
 import { FaCertificate } from 'react-icons/fa6';
+import { useEffect } from 'react';
 
-
-const Navbar = () => {
+const Navbar = ({theme, setTheme}) => {
     const cart = useSelector( (state) => state.cart);
     const wishlist = useSelector( (state) => state.wishlist);
+
+    function changeHandler(){
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+        sessionStorage.setItem("theme", newTheme);
+        window.dispatchEvent(new Event("theme-changed"));
+    }
   return (
     <div>
     <div className='flex flex-row justify-between items-center h-20 max-w-6xl mx-auto '>
@@ -19,12 +26,12 @@ const Navbar = () => {
     </NavLink>
 
     <NavLink to="/wishlist">
-      <FaBookSkull className='text-[white] translate-x-[-11rem] text-2xl'/>
+      <FaBookSkull className='text-[white] text-2xl -translate-x-2'/>
       {
-        wishlist.length > 0  && <span className='absolute left-[30rem] top-[1.4rem] bg-green-600 text-xs w-5 h-5 flex justify-center items-center animate-bounce rounded-full text-white'>{wishlist.length}</span>
+        wishlist.length > 0  && <span  className='absolute top-4 bg-green-600 text-xs w-5 h-5 flex justify-center items-center animate-bounce rounded-full text-white'>{wishlist.length}</span>
       }
     </NavLink>
-    <FaCertificate className='text-[white] text-2xl translate-x-[-24rem]'/>
+    <FaCertificate onClick={changeHandler} className='text-[white] text-2xl '/>
     <div className='text-white flex items-center font-medium mr-5 space-x-6'>
     <NavLink to="/">
          <p>Home</p>
